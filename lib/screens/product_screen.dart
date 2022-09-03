7ca8d03fc3b2041b9ff4ce425061057d738f4710
Michal <mmarciniak299@gmail.com>
2022-09-04 00:59:14 +0200

    custom popup menu button

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/rendering.dart';
 import 'package:inzynierka/colors.dart';
 import 'package:inzynierka/models/product.dart';
 import 'package:inzynierka/elements/product_item.dart';
+import 'package:inzynierka/widgets/custom_popup_menu_button.dart';
 
 class CustomColorSelectionHandle extends TextSelectionControls {
   CustomColorSelectionHandle(this.handleColor) : _controls = materialTextSelectionControls;
@@ -99,53 +100,177 @@ class _ProductScreenState extends State<ProductScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: Theme.of(context).copyWith(
-        textSelectionTheme: Theme.of(context).textSelectionTheme.copyWith(
-              selectionHandleColor: Colors.black,
-              selectionColor: Colors.black26,
-              cursorColor: Colors.black,
-            ),
-      ),
-      child: SafeArea(
-        child: NestedScrollView(
-          floatHeaderSlivers: true,
-          headerSliverBuilder: (context, _) => [
-            SliverToBoxAdapter(
-              child: Container(
-                height: 48,
-                margin: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(100)),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        cursorColor: Colors.black,
-                        decoration: InputDecoration(
-                          // border: InputBorder.none,
-                          hintText: "Wyszukaj",
-                          prefixIcon: const Icon(Icons.search, color: Colors.black),
-                          suffixIcon:
-                          IconButton(onPressed: onFilterPress, icon: const Icon(Icons.filter_list, color: Colors.black)),
-                        ),
-                        controller: searchController,
-                        selectionControls: CustomColorSelectionHandle(Colors.black),
-                      ),
-                    ),
-                  ],
-                ),
+    return Material(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          textSelectionTheme: Theme.of(context).textSelectionTheme.copyWith(
+                selectionHandleColor: Colors.black,
+                selectionColor: Colors.black26,
+                cursorColor: Colors.black,
               ),
-            )
-          ],
-          body: ListView.separated(
-            padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-            itemCount: productsList.length,
-            separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 16),
-            itemBuilder: (BuildContext context, int index) => ProductItem(product: productsList[index]),
+        ),
+        child: SafeArea(
+          child: NestedScrollView(
+            floatHeaderSlivers: true,
+            headerSliverBuilder: (context, _) => [
+              SliverToBoxAdapter(
+                child: Container(
+                  height: 48,
+                  margin: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColorLight,
+                    borderRadius: const BorderRadius.all(Radius.circular(100)),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          cursorColor: Colors.black,
+                          decoration: InputDecoration(
+                            // border: InputBorder.none,
+                            hintText: "Wyszukaj",
+                            prefixIcon: const Icon(Icons.search, color: Colors.black),
+                            suffixIcon: FilterButton(),
+                          ),
+                          controller: searchController,
+                          selectionControls: CustomColorSelectionHandle(Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+            body: ListView.separated(
+              padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+              itemCount: productsList.length,
+              separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 16),
+              itemBuilder: (BuildContext context, int index) => ProductItem(product: productsList[index]),
+            ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class Filter {
+  const Filter(this.name, this.value);
+
+  final String name;
+  final String value;
+}
+
+const _filters = [
+  Filter('Zweryfikowano', 'verified'),
+  Filter('Niezweryfikowano', 'unverified'),
+  Filter('Brak propozycji', ''),
+];
+
+class FilterButton extends StatelessWidget {
+  const FilterButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPopupMenuButton(
+      icon: Icon(Icons.filter_list),
+      color: Theme.of(context).scaffoldBackgroundColor,
+      shape: Theme.of(context).cardTheme.shape,
+      itemBuilder: (context) => [
+        GenericPopupMenuItem(
+          type: PopupMenuItemType.presentation,
+          child: Text('Segregacja'),
+        ),
+        for (var filter in _filters)
+          GenericPopupMenuItem(
+            child: Row(
+              children: [
+                Radio(
+                  value: filter,
+                  groupValue: _filters.first,
+                  onChanged: (val) {},
+                  activeColor: AppColors.positive,
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  visualDensity: VisualDensity.compact,
+                ),
+                SizedBox(width: 8.0),
+                Text(filter.name),
+              ],
+            ),
+          ),
+      ],
+    );
+  }
+}
+
+enum PopupMenuItemType {
+  presentation,
+  action,
+}
+
+class GenericPopupMenuItem<T> extends PopupMenuItem<T> {
+  const GenericPopupMenuItem({
+    super.key,
+    super.value,
+    super.enabled,
+    super.padding,
+    super.height,
+    super.mouseCursor,
+    super.child,
+    this.type = PopupMenuItemType.action,
+  });
+
+  final PopupMenuItemType type;
+
+  @override
+  PopupMenuItemState<T, GenericPopupMenuItem<T>> createState() => _GenericPopupMenuItemState<T>();
+}
+
+class _GenericPopupMenuItemState<T> extends PopupMenuItemState<T, GenericPopupMenuItem<T>> {
+  @override
+  void handleTap() {
+    widget.onTap?.call();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final PopupMenuThemeData popupMenuTheme = PopupMenuTheme.of(context);
+    final bool isClickable = widget.type == PopupMenuItemType.action;
+    TextStyle style = widget.textStyle ?? popupMenuTheme.textStyle ?? theme.textTheme.subtitle1!;
+
+    if (!widget.enabled) {
+      style = style.copyWith(color: theme.disabledColor);
+    }
+
+    Widget item = AnimatedDefaultTextStyle(
+      style: style,
+      duration: kThemeChangeDuration,
+      child: Container(
+        alignment: AlignmentDirectional.centerStart,
+        constraints: BoxConstraints(minHeight: widget.height),
+        padding: widget.padding ?? EdgeInsets.symmetric(horizontal: isClickable ? 8.0 : 16.0),
+        child: buildChild(),
+      ),
+    );
+
+    if (!widget.enabled) {
+      final bool isDark = theme.brightness == Brightness.dark;
+      item = IconTheme.merge(
+        data: IconThemeData(opacity: isDark ? 0.5 : 0.38),
+        child: item,
+      );
+    }
+
+    return MergeSemantics(
+      child: Semantics(
+        enabled: widget.enabled,
+        button: true,
+        child: InkWell(
+          onTap: (widget.enabled && isClickable) ? handleTap : null,
+          canRequestFocus: widget.enabled,
+          child: item,
         ),
       ),
     );
