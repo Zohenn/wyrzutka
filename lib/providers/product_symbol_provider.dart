5a6f5c9d297e8162145aa8d453c695b90fbb886d
Michal <mmarciniak299@gmail.com>
2022-10-04 15:35:36 +0200

    use CacheNotifier in ProductSymbolRepository

--- lib/providers/product_symbol_provider.dart
+++ lib/providers/product_symbol_provider.dart
@@ -2,13 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:inzynierka/data/static_data.dart';
 import 'package:inzynierka/models/product_symbol/product_symbol.dart';
+import 'package:inzynierka/providers/cache_notifier.dart';
 
 final _productSymbolCollection = FirebaseFirestore.instance.collection('symbols').withConverter(
       fromFirestore: ProductSymbol.fromFirestore,
       toFirestore: ProductSymbol.toFirestore,
     );
 
-final productSymbolRepositoryProvider = Provider((ref) => ProductSymbolRepository());
+final _productSymbolCacheProvider = createCacheProvider<ProductSymbol>();
+
+final productProvider = createCacheItemProvider(_productSymbolCacheProvider);
+
+final productSymbolRepositoryProvider = Provider((ref) => ProductSymbolRepository(ref));
 
 Future saveExampleSymbolData() async {
   return Future.wait(symbols.map((e) {
@@ -17,41 +22,15 @@ Future saveExampleSymbolData() async {
   }));
 }
 
-class ProductSymbolRepository {
-  final Map<String, ProductSymbol> cache = {};
+class ProductSymbolRepository with CacheNotifierMixin<ProductSymbol> {
+  ProductSymbolRepository(this.ref);
 
-  Future<ProductSymbol?> fetchId(String id) async {
-    if (cache[id] != null) {
-      return cache[id];
-    }
-    final snapshot = await _productSymbolCollection.doc(id).get();
-    final symbol = snapshot.data();
-    _addToCache(symbol);
-    return symbol;
-  }
+  @override
+  final Ref ref;
 
-  /// Will return only those ids that were fetched successfully or present in cache,
-  /// meaning the output list might be shorter than the input one.
-  Future<List<ProductSymbol>> fetchIds(List<String> ids) async {
-    final idsToFetch = ids.where((element) => cache[element] == null).toList();
-    if (idsToFetch.isNotEmpty) {
-      final snapshot = await _productSymbolCollection.where(FieldPath.documentId, whereIn: idsToFetch).get();
-      for (var element in snapshot.docs) {
-        _addToCache(element.data());
-      }
-    }
-    return ids
-        .map((e) {
-          return cache[e];
-        })
-        .where((element) => element != null)
-        .cast<ProductSymbol>()
-        .toList();
-  }
+  @override
+  CacheNotifier<ProductSymbol> get cache => ref.read(_productSymbolCacheProvider.notifier);
 
-  void _addToCache(ProductSymbol? symbol) {
-    if (symbol != null) {
-      cache[symbol.id] = symbol;
-    }
-  }
+  @override
+  CollectionReference<ProductSymbol> get collection => _productSymbolCollection;
 }
