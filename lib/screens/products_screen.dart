ac00f1c6f73eb1b8ba2b7b921f2b95efc1eb576b
Michal <mmarciniak299@gmail.com>
2022-10-08 13:49:49 +0200

    products screen cleanup

--- lib/screens/products_screen.dart
+++ lib/screens/products_screen.dart
@@ -7,8 +7,8 @@ import 'package:inzynierka/hooks/init_future.dart';
 import 'package:inzynierka/models/product/product.dart';
 import 'package:inzynierka/models/product/product_filters.dart';
 import 'package:inzynierka/providers/product_provider.dart';
-import 'package:inzynierka/providers/product_symbol_provider.dart';
 import 'package:inzynierka/screens/widgets/product_item.dart';
+import 'package:inzynierka/utils/async_call.dart';
 import 'package:inzynierka/utils/show_default_bottom_sheet.dart';
 import 'package:inzynierka/widgets/conditional_builder.dart';
 import 'package:inzynierka/widgets/custom_color_selection_handle.dart';
@@ -39,10 +39,11 @@ class ProductsScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final productRepository = ref.watch(productRepositoryProvider);
     final products = useState<List<Product>>([]);
     final future = useInitFuture<List<Product>>(
       () => ref.read(productsFutureProvider).then((value) {
-        products.value.addAll(value);
+        products.value = value;
         return value;
       }),
     );
@@ -54,16 +55,12 @@ class ProductsScreen extends HookConsumerWidget {
 
     useEffect(() {
       if (searchText.value.isNotEmpty) {
-        innerFuture.value = ref.read(productRepositoryProvider).search(searchText.value).then((value) {
-          products.value
-            ..clear()
-            ..addAll(value);
+        innerFuture.value = productRepository.search(searchText.value).then((value) {
+          products.value = value;
         });
       } else {
         innerFuture.value = ref.read(productsFutureProvider).then((value) {
-          products.value
-            ..clear()
-            ..addAll(value);
+          products.value = value;
           fetchedAll.value = false;
         });
       }
@@ -72,102 +69,90 @@ class ProductsScreen extends HookConsumerWidget {
     }, [searchText.value]);
 
     useEffect(() {
-      innerFuture.value = ref.read(productRepositoryProvider).fetchMore(filters: selectedFilters.value).then((value) {
-        products.value
-          ..clear()
-          ..addAll(value);
+      innerFuture.value = productRepository.fetchMore(filters: selectedFilters.value).then((value) {
+        products.value = value;
         fetchedAll.value = false;
       });
       return null;
     }, [selectedFilters.value]);
 
-    return Material(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      child: Theme(
-        data: Theme.of(context).copyWith(
-          textSelectionTheme: Theme.of(context).textSelectionTheme.copyWith(
-                selectionColor: Colors.black26,
+    return SafeArea(
+      child: FutureHandler(
+        future: future,
+        data: () => NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (context, _) => [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: _FilterSection(
+                  selectedFilters: selectedFilters.value,
+                  onFiltersChanged: (filters) => selectedFilters.value = filters,
+                  onSearch: (String value) => searchText.value = value,
+                ),
               ),
-        ),
-        child: SafeArea(
-          child: FutureHandler(
-            future: future,
-            data: () => NestedScrollView(
-              floatHeaderSlivers: true,
-              headerSliverBuilder: (context, _) => [
-                SliverToBoxAdapter(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: _FilterSection(
-                      selectedFilters: selectedFilters.value,
-                      onFiltersChanged: (filters) => selectedFilters.value = filters,
-                      onSearch: (String value) => searchText.value = value,
-                    ),
-                  ),
-                )
-              ],
-              body: FutureHandler(
-                future: innerFuture.value,
-                data: () => NotificationListener<ScrollNotification>(
-                  onNotification: (notification) {
-                    if (notification.metrics.extentAfter < 50.0 &&
-                        products.value.length >= 10 &&
-                        !fetchedAll.value &&
-                        !isFetchingMore.value) {
-                      isFetchingMore.value = true;
-                      final repository = ref.read(productRepositoryProvider);
-                      repository
+            )
+          ],
+          body: FutureHandler(
+            future: innerFuture.value,
+            data: () => NotificationListener<ScrollNotification>(
+              onNotification: (notification) {
+                if (notification.metrics.extentAfter < 50.0 &&
+                    products.value.length >= 10 &&
+                    !fetchedAll.value &&
+                    !isFetchingMore.value) {
+                  (() async {
+                    isFetchingMore.value = true;
+                    await asyncCall(
+                      context,
+                      () => productRepository
                           .fetchMore(filters: selectedFilters.value, startAfterDocument: products.value.last.snapshot!)
                           .then((value) {
                         products.value = [...products.value, ...value];
-                        // ref.read(_productsProvider.notifier).addProducts(value);
                         fetchedAll.value = value.length < ProductRepository.batchSize;
-                      }).catchError((err, stack) {
-                        debugPrint(err.toString());
-                        debugPrintStack(stackTrace: stack);
-                        // todo
-                      }).whenComplete(() => isFetchingMore.value = false);
-                    }
+                      }),
+                    );
+                    isFetchingMore.value = false;
+                  })();
+                }
 
-                    return false;
-                  },
-                  child: ConditionalBuilder(
-                    condition: products.value.isNotEmpty,
-                    ifTrue: () => ListView.separated(
-                      padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-                      itemCount: isFetchingMore.value || searchText.value.isNotEmpty
-                          ? products.value.length + 1
-                          : products.value.length,
-                      separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 16),
-                      itemBuilder: (BuildContext context, int index) => ConditionalBuilder(
-                        condition: index < products.value.length,
-                        ifTrue: () => ProductItem(product: products.value[index]),
-                        ifFalse: () => ConditionalBuilder(
-                          condition: searchText.value.isEmpty,
-                          ifTrue: () => const Center(child: CircularProgressIndicator()),
-                          ifFalse: () => Center(
-                            child: Text(
-                              'W przypadku wyszukiwania po nazwie wyświetlanych jest 5 najbardziej trafnych wyników.',
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context).textTheme.bodySmall,
-                            ),
-                          ),
+                return false;
+              },
+              child: ConditionalBuilder(
+                condition: products.value.isNotEmpty,
+                ifTrue: () => ListView.separated(
+                  padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+                  itemCount: isFetchingMore.value || searchText.value.isNotEmpty
+                      ? products.value.length + 1
+                      : products.value.length,
+                  separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 16),
+                  itemBuilder: (BuildContext context, int index) => ConditionalBuilder(
+                    condition: index < products.value.length,
+                    ifTrue: () => ProductItem(product: products.value[index]),
+                    ifFalse: () => ConditionalBuilder(
+                      condition: searchText.value.isEmpty,
+                      ifTrue: () => const Center(child: CircularProgressIndicator()),
+                      ifFalse: () => Center(
+                        child: Text(
+                          'W przypadku wyszukiwania po nazwie wyświetlanych jest 5 najbardziej trafnych wyników.',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.bodySmall,
                         ),
                       ),
                     ),
-                    ifFalse: () => Center(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SvgPicture.asset(
-                            'assets/images/empty_cart.svg',
-                            width: MediaQuery.of(context).size.width * 0.5,
-                          ),
-                          const SizedBox(height: 24.0),
-                          Text('Nie znaleziono produktów', style: Theme.of(context).textTheme.bodyLarge),
-                        ],
+                  ),
+                ),
+                ifFalse: () => Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/images/empty_cart.svg',
+                        width: MediaQuery.of(context).size.width * 0.5,
                       ),
-                    ),
+                      const SizedBox(height: 24.0),
+                      Text('Nie znaleziono produktów', style: Theme.of(context).textTheme.bodyLarge),
+                    ],
                   ),
                 ),
               ),
@@ -197,72 +182,80 @@ class _FilterSection extends HookWidget {
     final searchText = useState('');
     final debounce = useDebounceHook(onEmit: (value) => onSearch(value));
 
-    return Row(
-      children: [
-        Expanded(
-          child: TextField(
-            readOnly: selectedFilters.isNotEmpty,
-            controller: searchController,
-            cursorColor: Colors.black,
-            onChanged: (value) {
-              debounce.onChanged(value);
-              searchText.value = value;
-            },
-            decoration: InputDecoration(
-              hintText: 'Wyszukaj',
-              hintStyle: Theme.of(context).textTheme.subtitle1!.copyWith(color: Theme.of(context).hintColor),
-              // fillColor: Theme.of(context).primaryColorLight,
-              fillColor: selectedFilters.isEmpty ? Theme.of(context).primaryColorLight : Theme.of(context).dividerColor,
-              enabledBorder: Theme.of(context).inputDecorationTheme.enabledBorder!.copyWith(
-                    borderSide: BorderSide.none,
-                  ),
-              focusedBorder: Theme.of(context).inputDecorationTheme.focusedBorder!.copyWith(
-                    borderSide: BorderSide.none,
-                  ),
-              prefixIcon: const Icon(Icons.search, color: Colors.black),
-              suffixIcon: ConditionalBuilder(
-                condition: searchText.value.isEmpty,
-                ifTrue: () => IconButton(
-                  style: selectedFilters.isEmpty
-                      ? null
-                      : ButtonStyle(
-                          backgroundColor: const MaterialStatePropertyAll(Colors.white),
-                          side: MaterialStatePropertyAll(
-                            BorderSide(color: Theme.of(context).primaryColorLight, width: 2.0),
+    return Theme(
+      data: Theme.of(context).copyWith(
+        textSelectionTheme: Theme.of(context).textSelectionTheme.copyWith(
+              selectionColor: Colors.black26,
+            ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              readOnly: selectedFilters.isNotEmpty,
+              controller: searchController,
+              cursorColor: Colors.black,
+              onChanged: (value) {
+                debounce.onChanged(value);
+                searchText.value = value;
+              },
+              decoration: InputDecoration(
+                hintText: 'Wyszukaj',
+                hintStyle: Theme.of(context).textTheme.subtitle1!.copyWith(color: Theme.of(context).hintColor),
+                // fillColor: Theme.of(context).primaryColorLight,
+                fillColor:
+                    selectedFilters.isEmpty ? Theme.of(context).primaryColorLight : Theme.of(context).dividerColor,
+                enabledBorder: Theme.of(context).inputDecorationTheme.enabledBorder!.copyWith(
+                      borderSide: BorderSide.none,
+                    ),
+                focusedBorder: Theme.of(context).inputDecorationTheme.focusedBorder!.copyWith(
+                      borderSide: BorderSide.none,
+                    ),
+                prefixIcon: const Icon(Icons.search, color: Colors.black),
+                suffixIcon: ConditionalBuilder(
+                  condition: searchText.value.isEmpty,
+                  ifTrue: () => IconButton(
+                    style: selectedFilters.isEmpty
+                        ? null
+                        : ButtonStyle(
+                            backgroundColor: const MaterialStatePropertyAll(Colors.white),
+                            side: MaterialStatePropertyAll(
+                              BorderSide(color: Theme.of(context).primaryColorLight, width: 2.0),
+                            ),
                           ),
+                    onPressed: () async {
+                      final result = await showDefaultBottomSheet<Filters>(
+                        context: context,
+                        builder: (context) => FilterBottomSheet(
+                          groups: _filterGroups,
+                          selectedFilters: selectedFilters,
+                          single: true,
                         ),
-                  onPressed: () async {
-                    final result = await showDefaultBottomSheet<Filters>(
-                      context: context,
-                      builder: (context) => FilterBottomSheet(
-                        groups: _filterGroups,
-                        selectedFilters: selectedFilters,
-                        single: true,
-                      ),
-                    );
+                      );
 
-                    if (result != null) {
-                      onFiltersChanged(result);
-                    }
-                  },
-                  icon: const Icon(Icons.filter_list),
-                ),
-                ifFalse: () => IconButton(
-                  onPressed: () {
-                    debounce.cancel();
-                    searchText.value = '';
-                    searchController.text = '';
-                    onSearch('');
-                    FocusManager.instance.primaryFocus?.unfocus();
-                  },
-                  icon: const Icon(Icons.close),
+                      if (result != null) {
+                        onFiltersChanged(result);
+                      }
+                    },
+                    icon: const Icon(Icons.filter_list),
+                  ),
+                  ifFalse: () => IconButton(
+                    onPressed: () {
+                      debounce.cancel();
+                      searchText.value = '';
+                      searchController.text = '';
+                      onSearch('');
+                      FocusManager.instance.primaryFocus?.unfocus();
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
                 ),
               ),
+              selectionControls: CustomColorSelectionHandle(Colors.black),
             ),
-            selectionControls: CustomColorSelectionHandle(Colors.black),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
