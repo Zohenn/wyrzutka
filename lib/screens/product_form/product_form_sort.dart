90454c8edf27faade1ad5fed23bf1886d0b6b784
Michal <mmarciniak299@gmail.com>
2022-10-25 14:15:52 +0200

    allow for sort element to be filled from template

--- lib/screens/product_form/product_form_sort.dart
+++ lib/screens/product_form/product_form_sort.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:inzynierka/colors.dart';
+import 'package:inzynierka/hooks/init_future.dart';
 import 'package:inzynierka/models/product/sort_element.dart';
+import 'package:inzynierka/models/product/sort_element_template.dart';
+import 'package:inzynierka/providers/sort_element_template_provider.dart';
 import 'package:inzynierka/screens/product_form/product_form.dart';
 import 'package:inzynierka/utils/show_default_bottom_sheet.dart';
 import 'package:inzynierka/utils/validators.dart';
 import 'package:inzynierka/widgets/conditional_builder.dart';
+import 'package:inzynierka/widgets/future_handler.dart';
 import 'package:inzynierka/widgets/gutter_column.dart';
 import 'package:inzynierka/widgets/gutter_row.dart';
 import 'package:inzynierka/widgets/progress_indicator_button.dart';
@@ -308,72 +313,85 @@ class _ElementModel {
   String desc;
 }
 
-class _ElementSheet extends HookWidget {
+class _ElementSheet extends HookConsumerWidget {
   const _ElementSheet({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final sortElementTemplateRepository = ref.watch(sortElementTemplateRepositoryProvider);
+    final initFuture = useInitFuture(() => sortElementTemplateRepository.fetchAll());
+    final templates = ref.watch(allSortElementTemplatesProvider);
+    final templateItems =
+        useMemoized(() => templates.map((e) => DropdownMenuItem(value: e, child: Text(e.name))).toList(), [templates]);
     final element = useRef(_ElementModel('', ''));
+
     return SingleChildScrollView(
       padding: EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 16.0 + MediaQuery.of(context).viewInsets.bottom),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Nowy element',
-            style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.w500),
-          ),
-          const SizedBox(height: 16.0),
-          GutterColumn(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Nazwa',
-                ),
-                textInputAction: TextInputAction.next,
-                validator: Validators.required('Uzupełnij nazwę'),
-                onChanged: (value) => element.value.name = value,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Dodatkowe informacje',
-                ),
-                onChanged: (value) => element.value.desc = value,
-              ),
-              Center(
-                child: Text(
-                  'Lub',
-                  style: Theme.of(context).textTheme.labelLarge,
-                ),
-              ),
-              DropdownButtonFormField<dynamic>(
-                hint: const Text('Wybierz z listy'),
-                items: [],
-                onChanged: (v) {},
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.black,
-                    ),
-                    child: const Text('Cofnij'),
+      child: FutureHandler(
+        future: initFuture,
+        data: () => Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Nowy element',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(height: 16.0),
+            GutterColumn(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Nazwa',
                   ),
-                  TextButton(
-                    onPressed: () => Navigator.of(context).pop(element.value),
-                    style: TextButton.styleFrom(
-                      foregroundColor: AppColors.primaryDarker,
-                    ),
-                    child: const Text('Zapisz'),
+                  textInputAction: TextInputAction.next,
+                  validator: Validators.required('Uzupełnij nazwę'),
+                  onChanged: (value) => element.value.name = value,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Dodatkowe informacje',
                   ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                  onChanged: (value) => element.value.desc = value,
+                ),
+                Center(
+                  child: Text(
+                    'Lub',
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                ),
+                DropdownButtonFormField<SortElementTemplate>(
+                  hint: const Text('Wybierz z listy'),
+                  items: templateItems,
+                  onChanged: (v) {
+                    if (v != null) {
+                      Navigator.of(context).pop(_ElementModel(v.name, v.description ?? ''));
+                    }
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.black,
+                      ),
+                      child: const Text('Cofnij'),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pop(element.value),
+                      style: TextButton.styleFrom(
+                        foregroundColor: AppColors.primaryDarker,
+                      ),
+                      child: const Text('Zapisz'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
